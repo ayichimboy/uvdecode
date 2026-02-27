dc1042f6dd80a8af77381808e4b6d24243856f7b
ayichimboy <53350925+ayichimboy@users.noreply.github.com>
2026-02-26 22:57:57 -0600

    update data

---
+++ goproject/avatar.py
@@ -0,0 +1,9 @@
+import numpy as np            
+import pandas as pd              
+import seaborn as sns               
+import matplotlib.pyplot as plt   
+import os              
+
+
+# file location 
+file_path = 
